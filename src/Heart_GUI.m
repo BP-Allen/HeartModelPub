82562782b8d06397a0a2d71d6eadbda31a39b856
Benjamin Allen <ball907@UoA.auckland.ac.nz>
2026-05-20 15:13:04 +1200

    Adding the largest batch of files since Jan '26 slx files have been edited and added which contribute to the major issues here: - libs_unified.slx has be altered to appeal to the correct sensing   algorithm for milliseconds parameterisation - CLSfixed now includes various tests for different output methods - CLSfixed_nopace outputs EGMs which can then be passed into a function   generator - CLSfixed_pace includes simulated communication delays between the code - myocyteM/myocyteNM/pacemakerN slx files are different restitution   curve computation models which currently do not work to expectation - HeartGUI[_preset] are updated but are pseudo-deprecated - Major changes done with the editing app which now includes reading in   CellML action potential traces and limited probe editing   functionality.

--- src/Heart_GUI.m
+++ src/Heart_GUI.m
@@ -14,6 +14,8 @@ global ConfigGUI
 ConfigGUI.path_plot=[];
 ConfigGUI.cells=[];
 ConfigGUI.Node_pos=[];
+ConfigGUI.egm_outputs = {[]; []};
+ConfigGUI.egm_time = {[]; []};
 %% Link to the model
 ConfigGUI.modelName=modelName;
 ConfigGUI.mdl = mdl;
